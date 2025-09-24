53a17932eacffaae12a04d2003a393403ab91302
Julio Syah Putra <juliosp2107@gmail.com>
2025-09-24 16:51:02 +0700

    feat: react router and toaster set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Button } from "./components/ui/button";
+import toast from "react-hot-toast";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-teal-500 flex items-center justify-center">
-      <Button>Click me</Button>
+      <Button onClick={() => toast.success("Hello")}>Click me</Button>
     </div>
   );
 }
