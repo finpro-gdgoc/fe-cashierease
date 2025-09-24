6ce6dbed8cb37a52ad271f4113626349fd98f55d
Julio Syah Putra <juliosp2107@gmail.com>
2025-09-24 17:02:25 +0700

    feat: react query setup, inc dev tools.

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { Toaster } from "react-hot-toast";
+import QueryProvider from "./providers/QueryProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    <Toaster />
+    <QueryProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryProvider>
   </StrictMode>
 );
---
+++ src/providers/QueryProvider.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+const QueryProvider = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {import.meta.env.DEV ? <ReactQueryDevtools /> : null}
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "white",
+            color: "black",
+          },
+        }}
+      />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
