a9fb8b9307551870ee9adee83396b714d1ddfdc3
Julio Syah Putra <juliosp2107@gmail.com>
2025-09-24 16:38:43 +0700

    initialize canvas, vite + tailwind + shadcn

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-teal-500 flex items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
